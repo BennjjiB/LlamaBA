bb6b7bfe65f37f8c9a94c219b7068f574a14f6be
Benjamin Banzhaf <benni@slab.de>
2024-11-22 12:05:50 +0100

    fix

--- llama.py
+++ llama.py
@@ -22,9 +22,9 @@ class Llama3:
     ):
         user_prompt = message_history + [{"role": "user", "content": query}]
         tokenized_chat = self.tokenizer.apply_chat_template(
-            user_prompt, tokenize=False, add_generation_prompt=True
-        )
-        generated_ids = self.model.generate(tokenized_chat, max_new_tokens=max_tokens, return_tensors="pt") 
+            user_prompt, tokenize=False, add_generation_prompt=True, return_tensors="pt"
+        ).to(device)
+        generated_ids = self.model.generate(**tokenized_chat, max_new_tokens=max_tokens)
         response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
         return response, user_prompt + [{"role": "assistant", "content": response}]
 

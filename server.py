b657956cee0b5327a82b806381df6aefe447ccaa
Benjamin Banzhaf <benni@slab.de>
2025-01-10 15:56:14 +0100

    -

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ again unless it finished the task.
 app = Flask(__name__)
 # bot = GroqChatBot(model="llama-3.3-70b-specdec", setup_prompt=setup_prompt, tools=tool_definitions)
 bot = PandaChatBot(LLAMA_31_8, setup_prompt=setup_prompt)
-transcriber = Transcriber(model_type="large-v3", max_window_duration=0.5)
+transcriber = Transcriber(model_type="large-v3")
 
 
 @app.route("/")

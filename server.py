d7e75e2bfbf470a3d0a3a09bccf8f062c9fd08d7
Benjamin Banzhaf <benni@slab.de>
2025-01-10 16:03:10 +0100

    - changed llm for faster boot

--- server.py
+++ server.py
@@ -31,8 +31,8 @@ again unless it finished the task.
 """
 
 app = Flask(__name__)
-# bot = GroqChatBot(model="llama-3.3-70b-specdec", setup_prompt=setup_prompt, tools=tool_definitions)
-bot = PandaChatBot(LLAMA_31_8, setup_prompt=setup_prompt)
+bot = GroqChatBot(model="llama-3.3-70b-specdec", setup_prompt=setup_prompt, tools=tool_definitions)
+# bot = PandaChatBot(LLAMA_31_8, setup_prompt=setup_prompt)
 transcriber = Transcriber(model_type="large-v3")
 
 

8fcc1809cc2f30a3a6900543836d8755b1ad9d74
Benjamin Banzhaf <benni@slab.de>
2025-01-20 15:42:32 +0100

    -

--- server.py
+++ server.py
@@ -68,7 +68,7 @@ def get_llama_version() -> str:
                       carousel=True
                       )
     ]
-    result = inquirer.prompt(questions.get("Llama Model", '3.1 8B'))
+    result = inquirer.prompt(questions).get("Llama Model", '3.1 8B')
     if result == '3.1 8B':
         return LLAMA_31_8
     elif result == '3.3 70B':

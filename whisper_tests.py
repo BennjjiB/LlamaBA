5f453a3e44883c8f4175c6673d54a41f8868f962
Benjamin Banzhaf <benni@slab.de>
2025-01-20 16:52:27 +0100

    -

--- whisper_tests.py
+++ whisper_tests.py
@@ -6,7 +6,7 @@ SAMPLE_SIZE = 1
 transcriber = Transcriber()
 
 for i in range(0, SAMPLE_SIZE):
-    file_path = "test_audio/i.wav"
+    file_path = f"test_audio/{i}.wav"
     audio_data, sample_rate = sf.read(file_path)
     audio_data = transcriber.clean_audio(sample_rate, audio_data)
     transcription = transcriber.transcribe(audio_data)

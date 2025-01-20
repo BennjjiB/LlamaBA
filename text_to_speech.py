d374b0dc6f69cf72b7c5954b8d247481591d1f2e
Benjamin Banzhaf <benni@slab.de>
2025-01-20 15:01:35 +0100

    - added tts

---
+++ text_to_speech.py
@@ -0,0 +1,22 @@
+from models import build_model
+import torch
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+MODEL = build_model('kokoro-v0_19.pth', device)
+VOICE_NAME = [
+    'af', # Default voice is a 50-50 mix of Bella & Sarah
+    'af_bella', 'af_sarah', 'am_adam', 'am_michael',
+    'bf_emma', 'bf_isabella', 'bm_george', 'bm_lewis',
+    'af_nicole', 'af_sky',
+][0]
+VOICEPACK = torch.load(f'voices/{VOICE_NAME}.pt', weights_only=True).to(device)
+print(f'Loaded voice: {VOICE_NAME}')
+
+# 3️⃣ Call generate, which returns 24khz audio and the phonemes used
+from kokoro import generate
+text = "How could I know? It's an unanswerable question. Like asking an unborn child if they'll lead a good life. They haven't even been born."
+audio, out_ps = generate(MODEL, text, VOICEPACK, lang=VOICE_NAME[0])
+# Language is determined by the first letter of the VOICE_NAME:
+# 🇺🇸 'a' => American English => en-us
+# 🇬🇧 'b' => British English => en-gb
+
+print(out_ps)

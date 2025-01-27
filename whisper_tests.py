77f5280070ff650bed9a1f5ba1ef63d786b434fe
Benjamin Banzhaf <benni@slab.de>
2025-01-27 15:04:03 +0100

    -

--- whisper_tests.py
+++ whisper_tests.py
@@ -49,7 +49,7 @@ model_constant, model_result = get_whisper_model()
 transcriber = Transcriber(model_type=model_result)
 transcriptions = []
 targets = easy_prompt + neutral_prompt + hard_prompt + [""]*18
-len(targets)
+print(len(targets))
 duration = 0
 transcription_time = 0
 
